9b8c6e7f8827ee2aa1823f3ffaa892b788e898ed
Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>
2023-04-19 17:14:09 +0300

    Add WithDate option
    Signed-off-by: Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>

--- options.go
+++ options.go
@@ -1,8 +1,24 @@
 package currency
 
-//type OptionFunc func(c *Currency)
+import (
+	"fmt"
+	"time"
+)
 
-//func WithDate(day int, month int, year int) OptionFunc {
-// TODO Implement for custom dates
-//return func(c *Currency) {}
-//}
+type OptionFunc func(t *TCMB)
+
+// WithDate enables to pass custom date for past dates.
+func WithDate(day int, month time.Month, year int) OptionFunc {
+	return func(t *TCMB) {
+		d := fmt.Sprintf("%d", day)
+		m := fmt.Sprintf("%d", month)
+		if day < 10 {
+			d = "0" + d
+		}
+		if month < 10 {
+			m = "0" + m
+		}
+		date := fmt.Sprintf("%s%s%d", d, m, year)
+		t.date = date
+	}
+}
